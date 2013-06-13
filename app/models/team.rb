2073fe745e7805664ee92943e2c644e28e0ebd2b
Anujan Panchadcharam <anujan.panchadcharam@uoit.net>
2013-06-13 12:30:59 -0400

    Name validation for teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,5 @@
 class Team < ActiveRecord::Base
+	validates :name, uniqueness: true, length: {maximum: 30, minimum:4}
 	belongs_to :league
 	has_many :team_players #Think about this...
 	belongs_to :user
