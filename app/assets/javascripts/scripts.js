f9080f51163ad8fa00295f03a90b8eb2009abce1
Anujan Panchadcharam <anujan.panchadcharam@uoit.net>
2013-06-13 14:38:41 -0400

    More stuff!

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -71,21 +71,6 @@ $("[data-rel=popover]").popover();
 		
 		$('html, body').animate({scrollTop: 0}, 300);
 	})
-
-/***************************************************
-	IFRAME
-***************************************************/
-	$("iframe").each(function(){
-		var ifr_source = $(this).attr('src');
-		var wmode = "wmode=transparent";
-		if(ifr_source.indexOf('?') != -1) {
-		var getQString = ifr_source.split('?');
-		var oldString = getQString[1];
-		var newString = getQString[0];
-		$(this).attr('src',newString+'?'+wmode+'&'+oldString);
-		}
-		else $(this).attr('src',ifr_source+'?'+wmode);
-	});
 $('a[data-rel]').each(function() {
 $(this).attr('rel', $(this).attr('data-rel')).removeAttr('data-rel');
 });
